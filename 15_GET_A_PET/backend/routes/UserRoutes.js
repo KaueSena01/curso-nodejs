7948e64bb2309333aeb215d8b74671cdc486fff4
KaueSena01 <86299739+KaueSena01@users.noreply.github.com>
2022-01-20 18:55:16 -0300

    api do projeto

--- 15_GET_A_PET/backend/routes/UserRoutes.js
+++ 15_GET_A_PET/backend/routes/UserRoutes.js
@@ -5,10 +5,13 @@ const UserController = require('../controllers/UserController')
 // middleware
 const verifyToken = require('../helpers/verify-token')
 
+const { imageUpload } = require('../helpers/image-upload')
+// campo da imagem = image
+
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
+router.patch('/edit/:id', verifyToken, imageUpload.single("image"), UserController.editUser)
 
 module.exports = router
